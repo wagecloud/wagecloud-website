7a539b1685b0cfab73491628c4acaf086eeda5c2
Đậu Khoa <khoakomlem@gmail.com>
2025-06-17 00:28:29 +0700

    add generic memo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from 'clsx'
+import { memo } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const genericMemo: <T>(component: T) => T = memo
