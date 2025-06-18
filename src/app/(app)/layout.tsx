4afa42299e7ea879e8a131a7661d77499e8e9d52
Đậu Khoa <khoakomlem@gmail.com>
2025-06-19 01:42:30 +0700

    update app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,23 +1,30 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
 import { AlertModalProvider } from '@/components/modal/alert/alert-modal-provider'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
-import { getQueryClient } from '@/core/query-client'
-import { Sidebar } from '@/components/layout/sidebar'
+import { Toaster } from 'sonner'
+import { AppSidebar } from '@/components/layout/app-sidebar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { Separator } from '@/components/ui/separator'
 
 export default async function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const cookieStore = await cookies()
-  const token = cookieStore.get('token')
+  // TODO: remove this once the app is ready
+  await (new Promise((resolve) => {
+    // Simulate some async operation, e.g., fetching initial data
+    setTimeout(resolve, 2000)
+  }))
 
-  // if (!token) {
-  //   return redirect("/about");
-  // }
+  // TODO: ass code
+  if (typeof window !== 'undefined') {
+    const token = globalThis?.localStorage?.getItem?.('token')
 
-  const queryClient = getQueryClient()
+    if (!token?.length) {
+      window.location.href = '/login'
+    }
+  }
+
+  // const queryClient = getQueryClient()
 
   // look ma, no await
   // queryClient.prefetchQuery({
@@ -34,13 +41,25 @@ export default async function AppLayout({
   // });
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <AlertModalProvider>
-        <main className="flex flex-row overflow-y-auto">
-          <Sidebar />
-          <div className="p-6 grow">{children}</div>
-        </main>
-      </AlertModalProvider>
-    </HydrationBoundary>
+    <AlertModalProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="absolute flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 w-fit">
+            <div className="flex items-center gap-2 px-4">
+              {/* <SidebarTrigger className="-ml-1" /> */}
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              {/* <BreadcrumbNav /> */}
+            </div>
+          </header>
+          {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div> */}
+          <main className="flex flex-row h-screen">
+            <Toaster position="top-right" />
+            <div className="grow p-8">{children}</div>
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </AlertModalProvider>
+
   )
 }
