044ee9d607861bef3c7197a8be44a11614c7b622
Đậu Khoa <khoakomlem@gmail.com>
2025-06-19 01:40:16 +0700

    update instance card

--- src/app/(app)/components/instance-card.tsx
+++ src/app/(app)/components/instance-card.tsx
@@ -1,17 +1,22 @@
-import { Button } from '@/components/ui/button'
 import { TableCell, TableRow } from '@/components/ui/table'
-import { useStartInstance, useStopInstance } from '@/core/instance/instance.query'
+import { useGetInstanceMonitor, useStartInstance, useStopInstance } from '@/core/instance/instance.query'
 import {
   Play,
   Square,
   RefreshCw,
   Terminal,
+  Settings,
 } from 'lucide-react'
+import { ButtonLoading } from '@/components/ui/button-loading'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { InstanceStatus } from '@/core/instance/instance.type'
+import { getStatusBadge } from './status-badge'
+import { ErrorBoundary } from 'react-error-boundary'
 
 export function InstanceCard({
   id,
   name,
-  status,
   os_id,
   arch_id,
   cpu,
@@ -20,93 +25,82 @@ export function InstanceCard({
 }: Readonly<{
   id: string
   name: string
-  status: 'running' | 'stopped' | 'restarting'
   os_id: string
   arch_id: string
   cpu: number
   ram: number
   storage: number
 }>) {
+  const { data: monitor } = useGetInstanceMonitor(id)
   const { mutateAsync: mutateStartVM } = useStartInstance()
   const { mutateAsync: mutateStopVM } = useStopInstance()
 
-  const getStatusColor = (status: string) => {
-    switch (status) {
-      case 'running':
-        return 'bg-green-500'
-      case 'stopped':
-        return 'bg-red-500'
-      case 'restarting':
-        return 'bg-yellow-500'
-      default:
-        return 'bg-gray-500'
-    }
-  }
-
   return (
-    <TableRow
-      key={id}
-      className="cursor-pointer hover:bg-muted/50"
+    <ErrorBoundary fallback={(
+      <TableRow>
+        <TableCell colSpan={5} className="text-center">
+          Failed to load instance data adasd
+        </TableCell>
+      </TableRow>
+    )}
     >
-      <TableCell className="font-medium">{name}</TableCell>
-      <TableCell>
-        <div className="flex items-center gap-2">
-          <div
-            className={`h-2 w-2 rounded-full ${getStatusColor(status)}`}
-          />
-          <span className="capitalize">{status}</span>
-        </div>
-      </TableCell>
-      <TableCell className="hidden md:table-cell">
-        {os_id}
-        {' '}
-        {arch_id}
-      </TableCell>
-      <TableCell className="hidden lg:table-cell">
-        <div className="flex flex-col gap-1 text-xs">
-          <div>
-            {`CPU: ${cpu} core${cpu > 1 ? 's' : ''}`}
+      <TableRow
+        key={id}
+        className="cursor-pointer hover:bg-muted/50"
+      >
+        <TableCell className="font-medium">{name}</TableCell>
+        <TableCell>
+          <div className="flex items-center gap-2">
+            {getStatusBadge(monitor?.status ?? InstanceStatus.UNKNOWN)}
           </div>
-          <div>
-            {`RAM: ${ram} GB`}
+        </TableCell>
+        <TableCell className="hidden md:table-cell">
+          {os_id}
+          {' '}
+          {arch_id}
+        </TableCell>
+        <TableCell className="hidden lg:table-cell">
+          <div className="flex flex-col gap-1 text-xs">
+            <div>
+              {`CPU: ${cpu} core${cpu > 1 ? 's' : ''}`}
+            </div>
+            <div>
+              {`RAM: ${ram} GB`}
+            </div>
+            <div>
+              {`Storage: ${storage} GB`}
+            </div>
           </div>
-          <div>
-            {`Storage: ${storage} GB`}
-          </div>
-        </div>
-      </TableCell>
-      <TableCell className="text-right">
-        <div className="flex justify-end gap-2">
-          {status !== 'running' && (
-            <Button
+        </TableCell>
+        <TableCell className="text-right">
+          <div className="flex justify-end gap-2">
+            <ButtonLoading
               variant="outline"
               size="sm"
               onClick={async (e) => {
                 e.stopPropagation()
-                mutateStartVM(id)
+                await mutateStartVM(id)
               }}
               className="flex items-center gap-1"
+              disabled={monitor?.status === InstanceStatus.RUNNING || monitor?.status === InstanceStatus.UNKNOWN || !monitor}
             >
               <Play className="h-4 w-4" />
               Start
-            </Button>
-          )}
-          {status !== 'stopped' && (
-            <Button
+            </ButtonLoading>
+            <ButtonLoading
               variant="outline"
               size="sm"
               onClick={async (e) => {
                 e.stopPropagation()
-                mutateStopVM(id)
+                await mutateStopVM(id)
               }}
               className="flex items-center gap-1"
+              disabled={monitor?.status !== InstanceStatus.RUNNING}
             >
               <Square className="h-4 w-4" />
               Stop
-            </Button>
-          )}
-          {status !== 'restarting' && (
-            <Button
+            </ButtonLoading>
+            {/* <ButtonLoading
               variant="outline"
               size="sm"
               onClick={(e) => {
@@ -117,23 +111,36 @@ export function InstanceCard({
             >
               <RefreshCw className="h-4 w-4" />
               Restart
-            </Button>
-          )}
-          {status === 'running' && (
-            <Button
-              variant="default"
-              size="sm"
-              onClick={(e) => {
-                e.stopPropagation()
-              }}
-              className="flex items-center gap-1"
-            >
-              <Terminal className="h-4 w-4" />
-              SSH
-            </Button>
-          )}
-        </div>
-      </TableCell>
-    </TableRow>
+            </ButtonLoading> */}
+            {/* {status === 'running' && (
+              <ButtonLoading
+                variant="default"
+                size="sm"
+                onClick={(e) => {
+                  e.stopPropagation()
+                }}
+                className="flex items-center gap-1"
+              >
+                <Terminal className="h-4 w-4" />
+                SSH
+              </ButtonLoading>
+            )} */}
+            <Link href={`/instance/${id}`}>
+              <Button
+                variant="outline"
+                size="sm"
+                className="flex items-center gap-1"
+                disabled={monitor?.status === InstanceStatus.UNKNOWN || !monitor}
+              >
+                <Settings className="h-4 w-4" />
+                Manage
+                {' '}
+                {monitor?.status == InstanceStatus.UNKNOWN}
+              </Button>
+            </Link>
+          </div>
+        </TableCell>
+      </TableRow>
+    </ErrorBoundary>
   )
 }
