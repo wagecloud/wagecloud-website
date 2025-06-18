7e69fcfd89304ef49e34ff230ba6fa3f5369cdb7
Đậu Khoa <khoakomlem@gmail.com>
2025-06-19 01:40:31 +0700

    no more /create page

--- src/app/(app)/create/page.tsx
+++
@@ -1,8 +0,0 @@
-import type React from 'react'
-import { CreateInstanceForm } from './components/create-instance-form'
-
-export default function CreatePage() {
-  return (
-    <CreateInstanceForm></CreateInstanceForm>
-  )
-}
