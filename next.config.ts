3daca72df9a3cbfd179af2c93673dad428441f54
khoakomlem <khoakomlem@gmail.com>
2025-06-19 10:05:31 +0700

    update nextconfig

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Ignore ESLint errors during build
   },
+  allowedDevOrigins: ['wagecloud.ddns.net'],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['wagecloud.ddns.net'],
+    }
+  }
 }
 
 export default nextConfig
